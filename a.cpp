8b1f87e0fd157e8e6e45355dacb767b3c1008de9
lazic <lazic@qq.com>
2026-03-10 16:30:57 +0800

    [xiaohong] Finish my work

--- a.cpp
+++ a.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 
+using std::cout;
+using std::endl;
+
 void xiaoming(){
 
 }
 
 void xiaohong(){
-
-
+    std::string s = "Hello, my name is xiaohong.";
+    for(auto x:s)   
+        cout<<x;
+    cout<<endl;
+    return ;
 }
 
 
